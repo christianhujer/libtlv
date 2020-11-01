0ce8789f667a7a5537f6ddebd7b8069cb069a8ea
Christian Hujer <Christian.Hujer@nelkinda.com>
2020-11-01 13:15:16 +0530

    Fix several warnings found by clang tidy.

--- src/tlv.c
+++ src/tlv.c
@@ -2,32 +2,34 @@
 #include <assert.h>
 
 ssize_t getVNLength(const uint8_t **ptr, size_t *size) {
+    uint8_t c;
     size_t offset = 0;
     const uint8_t *p = *ptr;
-    size_t length = 0;
 
     if (offset >= *size)
         return -1;
-    uint8_t c = p[offset++];
-    if ((c & 0x80) == 0x00)
+    c = p[offset++];
+
+    size_t length;
+    if ((c & 0x80u) == 0x00)
         length = c;
     else {
         uint_fast8_t nonStartBytes;
-        if ((c & 0xE0) == 0xC0) { length = c & 0x1F; nonStartBytes = 1; } else
-        if ((c & 0xF0) == 0xE0) { length = c & 0x0F; nonStartBytes = 2; } else
-        if ((c & 0xF8) == 0xF0) { length = c & 0x07; nonStartBytes = 3; } else
-        if ((c & 0xFC) == 0xF8) { length = c & 0x03; nonStartBytes = 4; } else
-        if ((c & 0xFE) == 0xFC) { length = c & 0x01; nonStartBytes = 5; } else
-        { return -1; }
-        while (nonStartBytes-- > 0) {
+        if ((c & 0xE0u) == 0xC0) { length = c & 0x1Fu; nonStartBytes = 1; } else
+        if ((c & 0xF0u) == 0xE0) { length = c & 0x0Fu; nonStartBytes = 2; } else
+        if ((c & 0xF8u) == 0xF0) { length = c & 0x07u; nonStartBytes = 3; } else
+        if ((c & 0xFCu) == 0xF8) { length = c & 0x03u; nonStartBytes = 4; } else
+        if ((c & 0xFEu) == 0xFC) { length = c & 0x01u; nonStartBytes = 5; } else
+            return -1;
+        while (nonStartBytes-- > 0) { // NOLINT(readability-misleading-indentation)
             if (offset >= *size)
                 return -1;
             c = p[offset++];
-            if ((c & 0xC0) != 0x80)
+            if ((c & 0xC0u) != 0x80)
                 return -1;
 
-            length <<= 6;
-            length += c & 0x3F;
+            length <<= 6u;
+            length += c & 0x3Fu;
         }
     }
     assert(offset <= *size); // GCOVR_EXCL_LINE
